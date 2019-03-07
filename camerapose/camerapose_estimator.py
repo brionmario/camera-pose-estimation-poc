18c91a587a4a5ea864c2cbe568d9da2641954bc5
Brion Mario <brion.2015283@iit.ac.lk>
2019-03-07 20:18:42 +0530

    feat(camerapose): implement feature point matching in webcam stream :sparkles:

--- camerapose/camerapose_estimator.py
+++ camerapose/camerapose_estimator.py
@@ -14,26 +14,55 @@ def main():
     cv2.namedWindow('Video')
     cv2.moveWindow('Video', 20, 20)
 
+    prev_frame = None
+
     while True:
         # Capture frame-by-frame
         ret, frame = video_capture.read()
 
+        if prev_frame is not None:
+            # use previous frame here
+            pass
+
+        """
+        detect key feature points using sift
+        Requires - opencv-python==3.4.2.16, opencv-contrib-python==3.4.2.16
+        """
+        # sift = cv2.xfeatures2d.SIFT_create()
+        # kp_curr, des_curr = sift.detectAndCompute(frame, None)
+        # kp_prev, des_prev = sift.detectAndCompute(prev_frame, None)
+
+        """
+        detect key feature points using ORB
+        Requires - opencv-python==3.4.2.16, opencv-contrib-python==3.4.2.16
+        """
         # Initiate STAR detector
         orb = cv2.ORB_create()
 
-        # find the keypoints with ORB
-        kp = orb.detect(frame, None)
+        # find the keypoints and compute the descriptors with ORB for previous frame
+        kp_prev, des_prev = orb.detectAndCompute(prev_frame, None)
 
-        # compute the descriptors with ORB
-        kp, des = orb.compute(frame, kp)
+        # find the keypoints and compute the descriptors with ORB for current frame
+        kp_curr, des_curr = orb.detectAndCompute(frame, None)
 
-        # draw only keypoints location,not size and orientation
-        for marker in kp:
-            img2 = cv2.drawMarker(frame, tuple(int(i) for i in marker.pt), color=(0, 255, 0))
+        # create BFMatcher object
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+        # Match descriptors.
+        matches = bf.match(des_prev, des_curr)
+
+        # Sort them in the order of their distance.
+        matches = sorted(matches, key=lambda x: x.distance)
+
+        frame_mod = frame.copy()
+        frame_mod = cv2.drawMatches(prev_frame, kp_prev, frame, kp_curr, matches[:10], frame_mod, flags=2)
+
+        # save current frame
+        prev_frame = frame
 
         # Showing the frame and waiting
         # for the exit command
-        cv2.imshow('Video', frame)
+        cv2.imshow('Video', frame_mod)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
